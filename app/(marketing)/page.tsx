a68f2ece4ac0ae8a6bf0293defcf0009978d71d4
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-06-25 00:07:11 +0530

    added products page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,49 +1,82 @@
-"use client"
-import { ArrowRight, LayoutDashboard, ShoppingBag } from "lucide-react"
-import { Inter } from "next/font/google"
-import Image from "next/image"
-import { useEffect, useState } from "react"
+"use client";
+import { ArrowRight, LayoutDashboard, ShoppingBag } from "lucide-react";
+import { Inter } from "next/font/google";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const InterFont = Inter({
-    subsets : ['latin']
-})
-export default function page(){
-    const [message, setMessage] = useState("");
-    useEffect(()=>{
-        fetch('/api/hello').then((res)=>res.json()).then((data)=> setMessage(data.message))
-    }, [])
-    return (
-        <div className={`${InterFont.className} flex-col flex gap-4 my-12  justify-center items-center text-center w-full h-full `}>
-          <div className="bg-[#A5B4FC] rounded-full text-[#354484] font-semibold py-2 px-4">
-            Pastel Academy
+  subsets: ["latin"],
+});
+export default function page() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+  return (
+    <div
+      className={`${InterFont.className} flex-col flex gap-4 my-12  justify-center items-center text-center w-full h-full `}
+    >
+      <div className="bg-[#A5B4FC] rounded-full text-[#354484] font-semibold py-2 px-4">
+        Pastel Academy
+      </div>
+      <div className="text-5xl font-bold text-[#4b5a9c]">
+        Welcome to Soft Learning
+      </div>
+      <div className="text-[#454650] font-normal text-xl text-center max-w-250">
+        Explore the seamless intergration of custom API routes within a modern,
+        pastel-themed Next.js application. Learning has never looked so
+        approachable.
+      </div>
+      <div className="mt-8 flex flex-col justify-center items-center p-8 gap-4 bg-[#DDE1FF] rounded-xl">
+        <div className="relative w-20 h-20 bg-[#FFFFFF] rounded-full">
+          <Image
+            fill
+            src={"/images/hand-wave.svg"}
+            alt="hand-wave-icon"
+            className="absolute p-4"
+          />
+        </div>
+        <div className="font-semibold text-[#001354] text-4xl">
+          Message from API
+        </div>
+        <div className="bg-[#FFFFFF]  px-16 py-5 text-xl text-[#334282] rounded-lg">
+          "{message}"
+        </div>
+      </div>
+      <div className="sm:grid flex flex-col sm:grid-cols-2 gap-4 ">
+        <div className="col-span-1 transition-all duration-300 hover:scale-[105%]  flex items-start cursor-pointer group flex-col gap-2 p-4 justify-start text-start bg-white rounded-xl">
+          <div className=" bg-[#A6F2CF] rounded-full p-4">
+            <ShoppingBag className="text-[#247155]" size={32} />
           </div>
-          <div className="text-5xl font-bold text-[#4b5a9c]">
-            Welcome to Soft Learning
+          <div className="text-[#191c1E] text-2xl font-semibold">Products</div>
+          <div className="text-xl font-normal text-[#454650] max-w-120">
+            Browse our collection of soft, educational resources and courses.
           </div>
-          <div className="text-[#454650] font-normal text-xl text-center max-w-250">
-            Explore the seamless intergration of custom API routes within a modern, pastel-themed Next.js application. Learning has never looked so approachable.
-          </div>
-          <div className="mt-8 flex flex-col justify-center items-center p-8 gap-4 bg-[#DDE1FF] rounded-xl">
-            <div className="relative w-20 h-20 bg-[#FFFFFF] rounded-full">
-                <Image fill src={'/images/hand-wave.svg'} alt="hand-wave-icon" className="absolute p-4"  />
-            </div>
-            <div className="font-semibold text-[#001354] text-4xl">Message from API</div>
-            <div className="bg-[#FFFFFF]  px-16 py-5 text-xl text-[#334282] rounded-lg">"{message}"</div>
-          </div>
-          <div className="sm:grid flex flex-col sm:grid-cols-2 gap-4 ">
-            <div className="col-span-1 transition-all duration-300 hover:scale-[105%]  flex items-start cursor-pointer group flex-col gap-2 p-4 justify-start text-start bg-white rounded-xl">
-                <div className=" bg-[#A6F2CF] rounded-full p-4"><ShoppingBag className="text-[#247155]" size={32} /></div>
-                <div className="text-[#191c1E] text-2xl font-semibold">Products</div>
-                <div className="text-xl font-normal text-[#454650] max-w-120">Browse our collection of soft, educational resources and courses.</div>
-                <div className="text-[#247155]  flex gap-2">View Catalog <span className="group-hover:translate-x-2 transition-all duration-300"><ArrowRight /></span></div>
-            </div>
-                        <div className="col-span-1 transition-all duration-300 hover:scale-[105%]  flex items-start cursor-pointer group flex-col gap-2 p-4 justify-start text-start bg-white rounded-xl">
-                <div className=" bg-[#B9B3D9] rounded-full p-4"><LayoutDashboard className="text-[#494465]" size={32} /></div>
-                <div className="text-[#191c1E] text-2xl font-semibold">Dashboard</div>
-                <div className="text-xl font-normal text-[#454650] max-w-120">Browse our collection of soft, educational resources and courses.</div>
-                <div className="text-[#5F5A7C]  flex gap-2">View Catalog <span className="group-hover:translate-x-2 transition-all duration-300"><ArrowRight /></span></div>
-            </div>
+          <div className="text-[#247155]  flex gap-2">
+            View Catalog{" "}
+            <span className="group-hover:translate-x-2 transition-all duration-300">
+              <ArrowRight />
+            </span>
           </div>
         </div>
-    )
+        <div className="col-span-1 transition-all duration-300 hover:scale-[105%]  flex items-start cursor-pointer group flex-col gap-2 p-4 justify-start text-start bg-white rounded-xl">
+          <div className=" bg-[#B9B3D9] rounded-full p-4">
+            <LayoutDashboard className="text-[#494465]" size={32} />
+          </div>
+          <div className="text-[#191c1E] text-2xl font-semibold">Dashboard</div>
+          <div className="text-xl font-normal text-[#454650] max-w-120">
+            Browse our collection of soft, educational resources and courses.
+          </div>
+          <div className="text-[#5F5A7C]  flex gap-2">
+            View Catalog{" "}
+            <span className="group-hover:translate-x-2 transition-all duration-300">
+              <ArrowRight />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,96 @@
-export default function page (){
-    return (
-        <div>Hi I am a Product Page</div>
-    )
+import { ListFilter, Search, Star, Zap } from "lucide-react";
+import { Inter } from "next/font/google";
+import Image from "next/image";
+const InterFont = Inter({
+  subsets: ["latin"],
+});
+export default async function page({
+  searchParams,
+}: {
+  searchParams: Promise<{ page?: string }>;
+}) {
+  const { page } = await searchParams;
+  const currentPage = Number(page) || 1;
+  const res = await fetch("https://fakestoreapi.com/products");
+  const products = await res.json();
+  const totalPages = Math.ceil(products.length / 4);
+  const start = (currentPage - 1) * 4;
+  const paginatedProducts = products.slice(start, start + 4);
+  return (
+    <div className={`${InterFont.className} flex flex-col gap-8 mx-10 my-8`}>
+      <div className={`flex justify-between items-end text-center`}>
+        <div className="flex items-start justify-start gap-2 flex-col">
+          <div className="text-6xl text-[#191C1E] font-bold">
+            Study Essentials
+          </div>
+          <div className="text-2xl font-normal text-[#454650]">
+            Discover our curated collection of soft, aesthetic supplies
+          </div>
+          <div className="flex gap-2 px-3 text-xl py-2 bg-[#A5B4FC]/20 text-[#4B5A9C] font-medium rounded-full ">
+            <span>
+              <Zap />
+            </span>
+            <span>Lightning fast via Static Site Generation (SSG)</span>
+          </div>
+        </div>
+        <div className="flex  items-center gap-4">
+          <div className=" bg-[#F2F4F6]  flex gap-2 rounded-full px-4 py-3.5">
+            <Search className="text-[#767681]" />
+            <input
+              placeholder="Search supplies..."
+              className="outline-none placeholder:text-[#767681]"
+            />
+          </div>
+          <div className="bg-[#F2F4F6] cursor-pointer hover:bg-[#767681]/10 transition-all duration-300 text-[#4B5A9C] px-4 py-3.5 rounded-full">
+            <ListFilter />
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-12 place-items-center">
+        {paginatedProducts.map((item: any) => (
+          <div
+            className={"bg-white flex flex-col w-[300px] h-[450px] rounded-xl"}
+            key={item.id}
+          >
+            <Image
+              src={item.image}
+              alt={item.title}
+              width={120}
+              height={192}
+              className="bg-[#A5B4FC]/60 rounded-t-xl  w-full p-4 h-[195px] flex justify-start  items-start object-contain"
+            />
+            <div className={`p-6 flex flex-col flex-1 gap-4`}>
+              <div className={`text-[#5F5A7C] text-md `}>{item.category}</div>
+              <div className="text-2xl text-[#191C1E] font-bold">
+                {item.title}
+              </div>
+              <div className="flex gap-1  ">
+                {[...Array(5)].map((_, i) => (
+                  <Star
+                    key={i}
+                    size={20}
+                    fill={i < Math.round(item.rating.rate) ? "#1B6B4F" : "none"}
+                    className={
+                      i < Math.round(item.rating.rate)
+                        ? "text-[#1B6B4F]"
+                        : "text-[#767681]"
+                    }
+                  />
+                ))}
+                <div className="text-[#767681]">({item.rating.count})</div>
+              </div>
+              <div className="flex mt-auto justify-between items-center ">
+                <div className="text-[#4B5A9C] font-bold text-xl">
+                  ${item.price}
+                </div>
+                <div className="text-[#4B5A9C] cursor-pointer transition-all duration-300 hover:scale-[110%] font-medium text-md bg-[#A5B4FC]/30 py-2 px-4 rounded-full">
+                  Details
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
