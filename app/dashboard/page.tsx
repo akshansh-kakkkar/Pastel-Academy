51f35e657dcca6358ba91b1cac1754a8a2d8240f
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-06-26 13:26:06 +0530

    done with the dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,16 @@
 "use client";
-import { Loader2 } from "lucide-react";
+import { ChevronLeft, ChevronRight, Loader2 } from "lucide-react";
 import { Inter } from "next/font/google";
 import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import useSWR from "swr";
 const interFont = Inter({
   subsets: ["latin"],
 });
 export default function Page() {
-  const searchParams = useSearchParams;
-  const perPageItems =6;
-  const page = searchParams;
+  const searchParams = useSearchParams();
+  const perPageItems = 8;
+  const page = searchParams.get("page");
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
   const { data, error, isLoading } = useSWR(
     "https://jsonplaceholder.typicode.com/posts",
@@ -25,8 +26,9 @@ export default function Page() {
     "bg-pink-200 text-pink-700",
     "bg-amber-200 text-amber-700",
     "bg-sky-200 text-sky-700",
-  ]
-  const totalPages = data?.length / perPageItems;
+  ];
+  const router = useRouter()
+  const totalPages = Math.ceil((data?.length || 0 )/ perPageItems);
   const CurrentPage = Number(page) || 1;
   const startPage = (CurrentPage - 1) * perPageItems;
   const paginatedItems = data?.slice(startPage, startPage + perPageItems) || [];
@@ -54,18 +56,40 @@ export default function Page() {
         </div>
       </div>
       <div>
-      <div className="bg-[#F2F4F6]   px-6 py-5 border border-[#d4d4d4]  rounded-t-3xl">
-        <div className="font-bold text-2xl text-[#191C1E]">Recent Discussions</div>
-      </div>
-      <div className="border  flex justify-center flex-col gap-1  border-t-0 border-[#C6C6C6] rounded-t-none">
-        {paginatedItems.map((item : any)=>{
-          const color = colors[item.id % colors.length]
-          return(
-          <div key={item.id} className="border-b flex items-center  h-full w-full border-[#C6C6C6] py-6  px-12" >
-            <div className={` w-14 h-14 flex items-center text-center justify-center font-bold px-4 py-2 rounded-full text-2xl ${color}`}>{item.title.charAt(0).toUpperCase()}</div>
-          </div>)
-})}
-      </div>
+        <div className="bg-[#F2F4F6] px-12 py-8 border border-[#d4d4d4]  rounded-t-3xl">
+          <div className="font-bold text-3xl text-[#191C1E]">
+            Recent Discussions
+          </div>
+        </div>
+        <div className="border  rounded-3xl flex justify-center flex-col border-t-0 border-[#C6C6C6] rounded-t-none">
+          {paginatedItems.map((item: any) => {
+            const color = colors[item.id % colors.length];
+            return (
+              <div
+                key={item.id}
+                className="border-b gap-6 transition-all duration-300 cursor-pointer flex group items-center  h-full w-full border-[#C6C6C6] py-6  px-12"
+              >
+                <div
+                  className={` w-14 h-14 flex items-center text-center justify-center font-bold px-4 py-2 rounded-full text-2xl ${color}`}
+                >
+                  {item.title.charAt(0).toUpperCase()}
+                </div>
+                <div className="text-lg text-[#767681] font-medium group-hover:text-[#4B5A9C] capitalize">
+                  {item.body}
+                </div>
+              </div>
+            );
+          })}
+          <div className="bg-[#F2F4F6] gap-4 flex items-center justify-end text-center  px-12 py-8 rounded-b-3xl">
+            <button disabled={CurrentPage===1}  onClick={()=>{if(CurrentPage > 1){
+              router.push(`/dashboard?page=${CurrentPage - 1}`)
+            }}} className={`bg-white disabled:bg-white border border-[#C6C6C6] hover:text-white hover:bg-[#4b5a9c] text-[#191C1E] disabled:cursor-not-allowed disabled:text-[#191C1E] transition-all duration-300 cursor-pointer p-1 rounded-md`}><ChevronLeft size={24} /></button>
+            <div className="font-medium text-[#4B5A9C] text-xl">{CurrentPage} of {totalPages}</div>
+            <button disabled={totalPages === totalPages} onClick={()=>{if(CurrentPage < totalPages){
+              router.push(`/dashboard?page=${CurrentPage + 1}`)
+            }}} className={`bg-white cursor-pointer hover:bg-[#4b5a9c] hover:text-white transition-all duration-300 disabled:cursor-not-allowed disabled:bg-white disabled:text-[#191C1E] text-[#191C1E] border border-[#C6C6C6] p-1 rounded-md`}><ChevronRight size={24} /></button>
+          </div>
+        </div>
       </div>
     </div>
   );
